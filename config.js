3569a9244b7bc6ab42281c4bc50757d3f5a32bb7
Muebles y Sillones <ezedigital2021@gmail.com>
2026-07-04 12:13:05 -0300

    Usar anon key clásica; agregar vista previa

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // las políticas RLS (leer vendedores/reportes e insertar reportes).
 window.RV_CONFIG = {
   SUPABASE_URL: "https://agsniiuybebhmnbjfqix.supabase.co",
-  SUPABASE_KEY: "sb_publishable_pre_c8Cs8clkZrEupbcNdw__ip-hBxM",
+  SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnc25paXV5YmViaG1uYmpmcWl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NzM1MzUsImV4cCI6MjA5MzM0OTUzNX0.Ka34seOfKIzotQCug1lUTOcTtMttsezVpT11QQcZt2k",
   BUCKET: "rv-reportes",
 };
